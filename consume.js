9098a186bc481c0b92eaaf0bb7acd11a203a907e
Shengxu Quan <quans2@outlook.com>
2023-08-29 14:59:42 -0400

    use registry-schema and decrypt and decode from consumer

--- consume.js
+++ consume.js
@@ -1,23 +1,45 @@
-const { Kafka } = require('kafkajs');
+const { Kafka } = require("kafkajs");
+const { SchemaRegistry } = require("@kafkajs/confluent-schema-registry");
 
-const kafka = new Kafka({
-  clientId: 'my-client',
-  brokers: ['localhost:9092']
-});
+const crypto = require("crypto");
 
-const consumer = kafka.consumer({ groupId: 'my-group' });
+const registry = new SchemaRegistry({ host: "http://localhost:8081" });
 
 async function run() {
+  const kafka = new Kafka({
+    clientId: "my-consumer",
+    brokers: ["localhost:19092"],
+  });
+
+  const consumer = kafka.consumer({ groupId: "my-group" });
   await consumer.connect();
-  await consumer.subscribe({ topic: 'users', fromBeginning: true });
+  await consumer.subscribe({ topic: "users", fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        value: message.value.toString(),
-      });
+      const { sk, data } = JSON.parse(message.value.toString());
+      console.log(`encrypted data: ${data}`);
+      const decryptedData = decrypt(
+        Buffer.from(sk, "base64"),
+        Buffer.from(data, "base64")
+      );
+      console.log(`decryptedData: ${decryptedData}`);
+      const userJson = await registry.decode(decryptedData);
+      console.log(`decoded data: ${userJson}`);
     },
   });
+}
+
+function decrypt(secretKey, data) {
+  const algorithm = "aes-256-cbc";
+  const iv = data.slice(0, 16);
+  const encryptedData = data.slice(16);
+  const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
+  const decrypted = Buffer.concat([
+    decipher.update(encryptedData),
+    decipher.final(),
+  ]);
+  return decrypted;
 }
 
 run().catch(console.error);
